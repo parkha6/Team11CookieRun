df5dd9fd3d2250c6c53ab5c1c5d6d84a4487cdd3
Park ha <115542242+parkha6@users.noreply.github.com>
2025-10-29 17:58:25 +0900

    fix 싱글톤 매니저 추가

--- Team11CookieRun/Assets/01.Scripts/GameManager/UIManager.cs
+++ Team11CookieRun/Assets/01.Scripts/GameManager/UIManager.cs
@@ -2,6 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : SingletonManager<GameManager>
+public class UIManager : SingletonManager<UIManager>
 {
 }
