32f0f7a22facd08a56331f6305d8a698f077843b
Park ha <115542242+parkha6@users.noreply.github.com>
2025-10-29 19:06:13 +0900

    feat 스켈레톤 코드 삽입

--- Team11CookieRun/Assets/01.Scripts/GameManager/UIManager.cs
+++ Team11CookieRun/Assets/01.Scripts/GameManager/UIManager.cs
@@ -1,21 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-public class UIManager : SingletonManager<UIManager>
+public class UIManager : SingletonManager<UIManager>//UI에 표시되는 변수와 관련되어있는 클래스
 {
-    const string LevelKey = "Level";
+    const string levelKey = "Level";
     const string highScoreKey = "High Score";
     const string expKey = "Current Exp";
     const string hpKey = "Current Hp";
 
     int playerLevel = 1; //플레이어 레벨
+    internal int PlayerLevel
+    {
+        get { return playerLevel; }
+        private set
+        {
+            if (value <= 0)
+            { value = 0; }
+            playerLevel = value;
+        }
+    }
     int Exp { get { return 100 * playerLevel; } }//총 경험치 계산
     int currentExp = 0;//현재 Exp
     internal int CurrentExp
     {
         get { return currentExp; }
-        set
+        private set
         {
             if (value <= 0)
             { value = 0; }
@@ -27,28 +34,51 @@ public class UIManager : SingletonManager<UIManager>
     internal int CurrentHp
     {
         get { return currentHp; }
-        set
+        private set
         {
             if (value <= 0)
             { value = 0; }
+            else if (value > Hp)
+            { value = Hp; }
             currentHp = value;
         }
     }
     int score = 0; //점수
-    int highestScore = 0;//최고 점수 
-    private void Awake()
+    int highScore = 0;//최고 점수 
+    internal void LoadKey()//
     {
-        LoadKey();
+        if (PlayerPrefs.HasKey(levelKey))
+        { PlayerLevel = PlayerPrefs.GetInt(levelKey, 0); }
+        if (PlayerPrefs.HasKey(highScoreKey))
+        { highScore = PlayerPrefs.GetInt(highScoreKey, 0); }
+        if (PlayerPrefs.HasKey(expKey))
+        { currentExp = PlayerPrefs.GetInt(expKey, 0); }
+        if (PlayerPrefs.HasKey(hpKey))
+        { currentHp = PlayerPrefs.GetInt(hpKey, 0); }
     }
-    private void Update()
+    internal void CheckLevelUp()//레벨업 함수. int를 리턴함
     {
         if (CurrentExp >= Exp)
-        { playerLevel = LevelUp(); }
+        {
+            ++PlayerLevel;
+            PlayerPrefs.SetInt(levelKey, PlayerLevel);
+        }
     }
-    void LoadKey()
-    {//키값 불러오기  
+    internal void SetExp(int getAmount)//경험치 추가 함수
+    {
+        CurrentExp += getAmount;
+        PlayerPrefs.SetInt(expKey, currentExp);
     }
-    internal int LevelUp()
-    { return ++playerLevel; }
-
+    internal void SetHp(int getAmount)//음수를 넣으면 데미지 아닐까?
+    {
+        currentHp += getAmount;
+        PlayerPrefs.SetInt(hpKey, currentHp);
+    }
+    internal void SetScore(int getAmount)
+    { score += getAmount; }
+    internal void CompareScore()//게임이 끝나면 쓰는 함수
+    {
+        if (score > highScore)
+        { PlayerPrefs.SetInt(highScoreKey, score); }
+    }//또 뭐 필요할까...
 }
