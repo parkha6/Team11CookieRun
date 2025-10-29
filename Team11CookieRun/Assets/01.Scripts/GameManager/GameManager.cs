71b12db55e86a250ce68095e4ba26ca49bdc11bd
Park ha <115542242+parkha6@users.noreply.github.com>
2025-10-30 08:43:32 +0900

    style 게임 매니저 코드 정리

--- Team11CookieRun/Assets/01.Scripts/GameManager/GameManager.cs
+++ Team11CookieRun/Assets/01.Scripts/GameManager/GameManager.cs
@@ -1,9 +1,11 @@
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public enum GameStage
 {
+    Waiting,
     Start,
     Pause,
     End,
@@ -17,6 +19,8 @@ public class GameManager : SingletonManager<GameManager>
     GameObject EndUi;
     [SerializeField]
     GameObject PauseUi;
+    [SerializeField]
+    Button startButton;
     [SerializeField]
     Button pauseButton;
     [SerializeField]
@@ -33,59 +37,17 @@ public class GameManager : SingletonManager<GameManager>
         AddOnClickButton();
     }
     private void Start()
-    { StartGame(); }
-    private void Update()
-    {
-        if (UIManager.Instance.CurrentHp <= 0)
-        { GameOver(); }
-    }
-    void AddOnClickButton()
-    {
-        pauseButton.onClick.AddListener(OnClickGamePause);
-        pauseStartButton.onClick.AddListener(StartGame);
-        restartButton.onClick.AddListener(OnClickRestart);
-        quitButton.onClick.AddListener(QuitGame);
-    }
-    void OnClickGamePause()
-    {
-        if (currentStage != GameStage.End)
-        {
-            currentStage = GameStage.Pause;
-            SetState();
-        }
-    }
-    void GameOver()
-    {
-        currentStage = GameStage.End;
-        SetState();
-    }
-    void OnClickRestart()
-    {
-        if (currentStage == GameStage.End)
-        { SceneManager.LoadScene(sceneName); }
-    }//씬을 완전히 새로 시작?
-    void StopTime()
-    {
-        if (currentStage != GameStage.Pause)
-        { Time.timeScale = 0; }
-    }
-    void StartGame()
-    {
-        if (PauseUi.activeInHierarchy)
-        { PauseUi.SetActive(false); }
-        if (EndUi.activeInHierarchy)
-        { EndUi.SetActive(false); }
-        Time.timeScale = 1.0f;
-        currentStage = GameStage.Start;
-    }
-    void SetState()
     {
         switch (currentStage)
         {
+            case GameStage.Waiting:
+                WaitGame();
+                break;
             case GameStage.Start:
+                StartGame();
                 break;
             case GameStage.Pause:
-                StopTime(); 
+                StopTime();
                 PauseUi.SetActive(true);
                 break;
             case GameStage.End:
@@ -97,7 +59,69 @@ public class GameManager : SingletonManager<GameManager>
             default:
                 break;
         }
-    }  
+        StartGame();
+    }
+    private void Update()
+    {
+        switch (currentStage)
+        {
+            case GameStage.Waiting:
+                break;
+            case GameStage.Start:
+                if (UIManager.Instance.CurrentHp <= 0)
+                { currentStage = GameStage.End; }
+                break;
+            case GameStage.Pause:
+                break;
+            case GameStage.End:
+                break;
+            case GameStage.Unknown:
+            default:
+                break;
+        }
+
+    }
+    void AddOnClickButton()
+    {
+        startButton.onClick.AddListener(OnClickStart);
+        pauseButton.onClick.AddListener(OnClickGamePause);
+        pauseStartButton.onClick.AddListener(StartGame);
+        restartButton.onClick.AddListener(OnClickRestart);
+        quitButton.onClick.AddListener(QuitGame);
+    }
+    void WaitGame()
+    {
+        if (PauseUi.activeInHierarchy)
+        { PauseUi.SetActive(false); }
+        if (EndUi.activeInHierarchy)
+        { EndUi.SetActive(false); }
+    }
+    void OnClickStart()
+    { currentStage = GameStage.Start; }
+    void StartGame()
+    {
+        if (PauseUi.activeInHierarchy)
+        { PauseUi.SetActive(false); }
+        if (EndUi.activeInHierarchy)
+        { EndUi.SetActive(false); }
+        Time.timeScale = 1.0f;
+    }
+    void OnClickGamePause()
+    {
+        if (currentStage != GameStage.End)
+        { currentStage = GameStage.Pause; }
+    }
+    void OnClickRestart()
+    {
+        if (currentStage == GameStage.End)
+        { SceneManager.LoadScene(sceneName); }
+    }//씬을 완전히 새로 시작?
+
+    void StopTime()
+    {
+        if (currentStage != GameStage.Pause)
+        { Time.timeScale = 0; }
+    }
     internal void SaveGame()
     { PlayerPrefs.Save(); }
     internal void QuitGame()//게임 종료 함수
