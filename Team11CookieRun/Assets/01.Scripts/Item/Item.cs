e794259309925ffa49f945c08e76e8b05f5f8fd3
BAEKSEONGHYEON <01075723727a@gmail.com>
2025-10-30 11:27:01 +0900

    아이템 프리팹 추가

---
+++ Team11CookieRun/Assets/01.Scripts/Item/Item.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum ItemType { Coin, Heal, Invincible }
+    public ItemType itemType;
+
+    //아이템 수치 설정
+    public float value = 0f;     // 코인 점수 or 회복량(%)
+    public float duration = 5f;  // 무적 지속 시간
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Player player = collision.GetComponent<Player>();
+            if (player != null)
+                ApplyEffect(player);
+
+            Destroy(gameObject); // 아이템 먹으면 사라짐
+        }
+    }
+
+    void ApplyEffect(Player player)
+    {
+        switch (itemType)
+        {
+            case ItemType.Coin:
+                player.AddScore((int)value);
+                break;
+
+            case ItemType.Heal:
+                player.HealPercent(value);
+                break;
+
+            case ItemType.Invincible:
+                player.ActivateInvincibility(duration);
+                break;
+        }
+    }
+}
