6f5c60ea9d3a41678a5b0c18881e94e6157f0a79
Kimtaehwan <coseltm123@gmail.com>
2025-10-30 10:35:47 +0900

    Set : 예시 메인메뉴 수정

---
+++ Team11CookieRun/Assets/01.Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public string GameSceneName = "TestUIScene";
+    public void StartGame()
+    {
+        // "SampleScene"이라는 이름의 씬으로 이동합니다.
+        SceneManager.LoadScene("TestUIScene");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("게임 종료 버튼이 클릭되었습니다.");
+
+        // 실제 빌드된 게임(PC, 모바일 등)에서 게임을 종료시킵니다.
+        Application.Quit();
+    }
+
+}
+
