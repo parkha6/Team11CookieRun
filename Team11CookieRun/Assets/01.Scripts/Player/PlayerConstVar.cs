4fd83bf380fad56722fd0e9fc94a063254bff746
youchan97 <151013695+youchan97@users.noreply.github.com>
2025-10-30 17:07:11 +0900

    Refactor : 플레이어 애니메이션 스트링 -> Const로 사용

---
+++ Team11CookieRun/Assets/01.Scripts/Player/PlayerConstVar.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerConstVar
+{
+    public const string IdleCondition = "Idle";
+    public const string JumpCondition = "IsJump";
+    public const string SlideCondition = "IsSlide";
+    public const string SlideUpCondition = "SlideUp";
+    public const string RunCondition = "IsRun";
+}
