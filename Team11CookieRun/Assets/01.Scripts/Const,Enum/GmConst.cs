ef7573e4285f3c1fe5a08853ae2ebfd34e641d76
Park ha <115542242+parkha6@users.noreply.github.com>
2025-11-01 11:58:46 +0900

    fix UI 최종수정. PauseUI 띄우는건 어떻게 된건지 몰라서 못 고쳤음.

--- Team11CookieRun/Assets/01.Scripts/Const,Enum/GmConst.cs
+++ Team11CookieRun/Assets/01.Scripts/Const,Enum/GmConst.cs
@@ -7,4 +7,6 @@ public class GmConst
     internal const string highScoreKey = "High Score";
     internal const byte minScore = 0;
     internal const byte dead = 0;
+    internal const byte stopTime = 0;
+    internal const byte runTime = 1;
 }
--- Team11CookieRun/Assets/01.Scripts/GameManager/GameManager.cs
+++ Team11CookieRun/Assets/01.Scripts/GameManager/GameManager.cs
@@ -24,7 +24,10 @@ public class GameManager : SingletonManager<GameManager>
     public bool IsPause { get { return isPause; } set { isPause = value; } }
     #endregion
     #region Life Cycle
-    protected override void Awake()//시작시점에 필요한 변수를 로드하게 만들었음.
+    /// <summary>
+    /// 게임이 맨 처음 재생될때 필요한 클래스들을 가져오고 PlayerPrefs를 로드함.
+    /// </summary>
+    protected override void Awake()
     {
         player = FindObjectOfType<Player>();
         uiManager = ScoreManager.Instance;
@@ -37,7 +40,8 @@ public class GameManager : SingletonManager<GameManager>
     internal void AddStartScene(GameUIManager startScene)
     { gameUIManager = startScene; }
     /// <summary>
-    /// 스위치 루프를 돌면서 currentStage의 값에 따라 업데이트를 돌린다.
+    /// 스위치 루프를 돌면서 currentStage의 값에 따라 업데이트를 돌린다. 
+    /// Start는 각 씬의 UI매니저에 있습니다.
     /// </summary>
     private void Update()
     {
@@ -52,11 +56,11 @@ public class GameManager : SingletonManager<GameManager>
                 { currentStage = GameStage.End; }
                 break;
             case GameStage.Pause:
-                StopTime();
+                ManageTime(GmConst.stopTime);
                 gameUIManager.ShowPauseUI();
                 break;
             case GameStage.End:
-                StopTime();
+                ManageTime(GmConst.stopTime);
                 gameUIManager.CompareScore(player.Score);
                 gameUIManager.ShowEndUI();
                 break;
@@ -67,10 +71,13 @@ public class GameManager : SingletonManager<GameManager>
     }
     #endregion
     #region Starting
-    internal void StartGame()//게임이 시작될때 세팅.
+    /// <summary>
+    /// 게임이 시작될때 일시정지나 게임 완료 같은 UI를 한번 숨기고 시간을 재생한다.
+    /// </summary>
+    internal void StartGame()
     {
         gameUIManager.HideUi();
-        RunTime();
+        ManageTime(GmConst.runTime);
     }
     #endregion
     #region PauseGame
@@ -102,25 +109,48 @@ public class GameManager : SingletonManager<GameManager>
     }
     #endregion
     #region Utility
+    /// <summary>
+    /// HP를 초기화하고 IsDie bool을 풀고 현재 점수를 0으로 만드는 변수.
+    /// 근데 제가 임의로 이러니까 조작키가 안 먹어서 유찬님이 만들어주시는게 좋을듯.
+    /// </summary>
     internal void ResetValue()
     {
-        if (player.Hp <= GmConst.dead )
+        if (player.Hp <= GmConst.dead)
         { player.Hp = player.MaxHp; }
         if (player.IsDie)
         { player.IsDie = false; }
         if (player.Score > 0)
         { player.Score = GmConst.minScore; }
     }
+    /// <summary>
+    /// whichScene에 입력된 string값이랑 같은 제목의 씬으로 이동하는 함수.
+    /// </summary>
+    /// <param name="whichScene"></param>
     internal void MoveScene(string whichScene)
     { SceneManager.LoadScene(whichScene); }
-    void StopTime()//시간을 멈추는 함수
-    { Time.timeScale = 0; }
-    void RunTime()//시간을 재생하는 함수
-    { Time.timeScale = 1.0f; }
+    /// <summary>
+    /// inputTime의 값에 맞춰서 시간을 조작한다.
+    /// </summary>
+    /// <param name="inputTime"></param>
+    void ManageTime(float inputTime)
+    {
+        if (inputTime < 0)
+        { inputTime = 0; }
+        Time.timeScale = inputTime;
+    }
+    /// <summary>
+    /// 메모리에 있는 PlayerPrefs의 값을 하드에 저장한다.
+    /// </summary>
     internal void SaveGame()//게임 저장
     { PlayerPrefs.Save(); }
+    /// <summary>
+    /// PlayerPrefs의 값을 모두 제거한다.
+    /// </summary>
     internal void DeleteData()
     { PlayerPrefs.DeleteAll(); }
+    /// <summary>
+    /// 에디터면 에디터를 종료하고 본게임이면 본게임을 종료한다.
+    /// </summary>
     internal void QuitGame()//게임 종료 함수
     {
         SaveGame();
