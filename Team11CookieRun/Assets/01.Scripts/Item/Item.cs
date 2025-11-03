5d0f3a4de5349f630622eb830bee5294dab8104f
youchan97 <151013695+youchan97@users.noreply.github.com>
2025-11-03 19:50:20 +0900

    Feat : 버프 쿨타임 연출 기능

--- Team11CookieRun/Assets/01.Scripts/Item/Item.cs
+++ Team11CookieRun/Assets/01.Scripts/Item/Item.cs
@@ -70,7 +70,7 @@ public class Item : MonoBehaviour
         {
             case ItemType.Coin: player.AddScore((int)value); break;
             case ItemType.Heal: player.HealPercent(value); break;
-            case ItemType.Invincible: player.ActivateInvincibility(duration); player.ObtainBuffItem(buffUi); break;
+            case ItemType.Invincible: player.ActivateInvincibility(duration); GameObject buff = player.GetBuffUi(buffUi);  player.ObtainBuffItem(buff); break;
             case ItemType.Slow: player.ApplySlow(value, duration); player.ObtainBuffItem(buffUi); break;
         }
     }
